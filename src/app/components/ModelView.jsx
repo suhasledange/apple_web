9e78f9afb49e26bc1402ee9f582a57ed01be86c5
suhasledange <suhasledange10@gmail.com>
2024-06-16 14:32:25 +0530

    asdf

---
+++ src/app/components/ModelView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModelView = () => {
+  return (
+    <div>
+      modelview
+    </div>
+  )
+}
+
+export default ModelView
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        blue:'#2997FF',
+        gray:{
+          DEFAULT:'#86868b',
+          100:'#94928d',
+          200:'#afafaf',
+          300:'#42424570',
+        },
+        zinc:'#101010'
       },
     },
   },
